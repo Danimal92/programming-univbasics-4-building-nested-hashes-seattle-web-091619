cb71b3d415ad1023e609e6cb25f016b52fba4fff
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-27 07:29:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
   },
     :names => {:reading_railroad => {},
-    :pennsylvania => {},
+    :pennsylvania_railroad => {},
     :b_and_o_railroad => {},
     :shortline_railroad => {}}
     
